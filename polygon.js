3de22c053c22ae1dc332832f01592a56f237545b
ywalakamar <barxwells@gmail.com>
2022-12-03 14:50:56 +0300

    Generator Method
    - use a polygon to demonstrate generator method in JS

---
+++ polygon.js
@@ -0,0 +1,15 @@
+/**Generator method */
+class Polygon {
+  constructor(...sides) {
+    this.sides = sides;
+  }
+
+  /**Generator method */
+  *getSides() {
+    for (const side of this.sides) {
+      yield side;
+    }
+  }
+}
+const pentagon = new Polygon(1, 2, 3, 4, 5);
+console.log(...pentagon.getSides());
